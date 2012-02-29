209d65e60602ea0d8ca893bd1fde7b81f6c862a5
Jarrod Chambers <jarrodchambers@gmail.com>
2012-02-28 21:37:15 -0500

    Added code to deal with scheduling conflicts, and sample data

--- fringe.rb
+++ fringe.rb
@@ -32,28 +32,34 @@ end
 # attended - whether or not a timeslot has been chosen for this play
 
 class Show
-	attr_accessor :num_avail, :title, :venue, :times, :attended
+	attr_accessor :num_avail, :title, :venue, :times, :attended, :time_attending
 	
 	def initialize(title, venue)
 		@title = title
 		@venue = venue
-		@num_avail = 0;
-		@attended = false;
-		@times = [];
+		@num_avail = 0
+		@attended = false
+		@times = []
+		@time_attending = nil
 	end
 	
 	def add_showtime(day, hour)
 		@times.push(Showtime.new(day, hour))
 		@num_avail += 1
 	end
+	
+	def is_showtime(day, hour)
+		@times.each do |time|
+			if (time.day == day && time.hour == hour)
+				return true
+			end
+		end
+		return false
+	end
 
 	def print_showtimes
 		@times.each do |time|
-#			if (time.attended) then	puts "Attending at this time:" end
-			if (time.attended)
-				puts "Attending at this time:"
-				puts "Playing at #{time.hour} on #{time.day}"
-			end
+			puts "Playing at #{time.hour} on #{time.day}"
 		end
 	end	
 	
@@ -75,37 +81,61 @@ class Show
 end
 
 # Recursive implementation of the scheduling algorithm
-# Will need failure handling in the case of a dead end in scheduling
-def schedule_next_show(schedule, index)
+def schedule_next_show(unscheduled, scheduled)
 	# Get first available showtime
-	this_time =	schedule[index].get_available
+	this_time =	unscheduled[0].get_available
 	
 	# If no time is available, return false to signal failure
-	if !this_time then return false end
+	if !this_time then 
+		# Find the play that has a common time with the most number of shows
+		# Simple because scheduled just happens to be sorted by number of slots
+		new_play = nil
+		scheduled.each do |play|
+			unscheduled[0].times.each do |time|
+				if play.is_showtime(time.day, time.hour)
+					this_time = time
+					new_play = play
+					break
+				end
+			end
+			break if this_time
+		end
+		
+		if !this_time
+			return false # We give up! This is not likely to happen. We're more likely to end up in an infinite loop...
+		end
+					
+		# Move that play to unscheduled and mark it unattended, and the common time unavailable
+		new_play.attended = false
+		new_play.time_attending = nil
+		new_play.mark_time_unavailable(this_time.day, this_time.hour)
+		unscheduled.push(scheduled.delete(new_play))
+	end
 	
 	# Mark this time as attended
 	this_time.mark_attended
-	schedule[index].attended = true
+	unscheduled[0].attended = true
+	unscheduled[0].time_attending = this_time
 
 	# Mark this slot as unattended for all other plays
-	schedule[index + 1..-1].each do |play|
-		# puts "Checking #{play.title}"
+	scheduled[0..-1].each do |play|
+		play.mark_time_unavailable(this_time.day, this_time.hour)
+	end
+	unscheduled[1..-1].each do |play|
 		play.mark_time_unavailable(this_time.day, this_time.hour)
 	end
 	
+	scheduled.push(unscheduled.shift)
+	
 	# Return true if we're done, otherwise schedule the next play
-	index += 1
-	if index == schedule.length
+	if 0 == unscheduled.length
 		return true
 	else
-		# Sort schedule by number of showtimes available
-		schedule = schedule.sort {|x,y| x.num_avail > y.num_avail ? 1 : -1}
-		return schedule_next_show(schedule, index)
+		return schedule_next_show(unscheduled, scheduled)
 	end
 end
 
 # Read shows from file in CSV format: title, venue, day, hour
-
 shows = Hash.new
 
 file = File.new("shows.txt", "r")
@@ -119,16 +149,26 @@ while (line = file.gets)
 end
 
 # Convert the hash to an array for easier manipulation
-schedule = shows.values
+unscheduled = shows.values
+scheduled = []
+
+puts "Attempting to schedule #{unscheduled.length} plays."
 
 # Sort by number of slots available
-schedule = schedule.sort {|x,y| x.num_avail > y.num_avail ? 1 : -1}
+unscheduled = unscheduled.sort {|x,y| x.num_avail > y.num_avail ? 1 : -1}
 
 # Keep scheduling until all the plays have slots assigned
-success = schedule_next_show(schedule, 0)
+success = schedule_next_show(unscheduled, scheduled)
+
+# Sort by date and time before printing out
+scheduled = scheduled.sort do |a,b|
+  comp = (a.time_attending.day <=> b.time_attending.day)
+  comp.zero? ? (a.time_attending.hour <=> b.time_attending.hour) : comp
+end
 
 # Print out plays
-schedule.each do |play|
+scheduled.each do |play|
 	puts play.title, play.venue
-	play.print_showtimes
+	puts "Time attending: #{play.time_attending.hour} on #{play.time_attending.day}"
 end
+puts "Scheduled #{scheduled.length} plays."
